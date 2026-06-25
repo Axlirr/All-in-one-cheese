24423bc65954f06e5b03f2584a01c1b706835890
Axlirr <arbazkhanofficial7860@gmail.com>
2026-06-25 14:02:22 +0800

    Support DISCORD_TOKEN as fallback

--- start.sh
+++ start.sh
@@ -9,5 +9,8 @@ export RED_INSTANCE_NAME=${RED_INSTANCE_NAME:-"renderbot"}
 # Run the smart setup script which handles the Postgres URI parsing
 python setup_red.py
 
+# Use RED_TOKEN, but fallback to DISCORD_TOKEN if it exists
+export ACTUAL_TOKEN=${RED_TOKEN:-$DISCORD_TOKEN}
+
 # Start the bot
-redbot "${RED_INSTANCE_NAME}" --token "${RED_TOKEN}" --prefix "${RED_PREFIX:-!}" --no-prompt
+redbot "${RED_INSTANCE_NAME}" --token "${ACTUAL_TOKEN}" --prefix "${RED_PREFIX:-!}" --no-prompt
